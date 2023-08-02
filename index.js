00802b3187ce561348ba817c674da871a4ac4d53
KarinaBertosh <cherrycake246@gmail.com>
2023-08-02 22:31:41 +0300

    refactor: code

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const email = "cherrycake246@gmail.com";
 const hashAll = [];
 
 const folder = fs.readdir("files", (err, files) => {
-  const buff = fs.readFileSync("index.js");
   files.forEach((f) => {
     const hash = crypto.createHash("sha3-256").update(f).digest("hex");
     hashAll.push(hash);
